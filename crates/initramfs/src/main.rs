0ef1eca80359bb67e0bcb07c849017202bcc0670
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2025-08-26 16:25:31 +0530

    Move composefs setup root to bootc initramfs
    Move the composefs-setup-root code from composefs-rs to bootc-initramfs
    crate
    
    Signed-off-by: Johan-Liebert1 <pragyanpoudyal41999@gmail.com>

--- crates/initramfs/src/main.rs
+++ crates/initramfs/src/main.rs
@@ -1,24 +1,15 @@
 //! Code for bootc that goes into the initramfs.
-//! At the current time, this is mostly just a no-op.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
+
+mod mount;
 
 use anyhow::Result;
 
-fn setup_root() -> Result<()> {
-    let _ = std::fs::metadata("/sysroot/usr")?;
-    println!("setup OK");
-    Ok(())
-}
+use clap::Parser;
+use mount::{gpt_workaround, setup_root, Args};
 
 fn main() -> Result<()> {
-    let v = std::env::args().collect::<Vec<_>>();
-    let args = match v.as_slice() {
-        [] => anyhow::bail!("Missing argument".to_string()),
-        [_, rest @ ..] => rest,
-    };
-    match args {
-        [] => anyhow::bail!("Missing argument".to_string()),
-        [s] if s == "setup-root" => setup_root(),
-        [o, ..] => anyhow::bail!(format!("Unknown command {o}")),
-    }
+    let args = Args::parse();
+    gpt_workaround()?;
+    setup_root(args)
 }
