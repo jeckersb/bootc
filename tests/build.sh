a3a28fdff2b69c5c118f01ed4c92effb4d9eaa13
Colin Walters <walters@verbum.org>
2025-09-12 13:03:43 -0400

    integration: Copy distro-sync from main Dockerfile
    Over in https://github.com/bootc-dev/bootc/pull/1607
    I actually *just* deduplicated this code, but that isn't
    ready to merge yet.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- tests/build.sh
+++ tests/build.sh
@@ -34,6 +34,7 @@ case \$ID in
     centos|rhel) dnf config-manager --set-enabled crb;;
     fedora) dnf -y install dnf-utils 'dnf5-command(builddep)';;
 esac
+dnf -y distro-sync ostree{,-libs} systemd
 dnf -y builddep contrib/packaging/bootc.spec
 dnf -y install git-core
 EORUN
